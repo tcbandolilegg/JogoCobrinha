0e8bb74b83eabee54b1b9bf8e170439324a99023
Thereza Bandoli <tcbandolilegg@gmail.com>
2020-09-23 22:21:03 -0300

    Update script.js
    corrigindo o erro de sair da tela

--- js/script.js
+++ js/script.js
@@ -10,8 +10,7 @@ snake[0] = {
   y: 8 * box
 };
 
-console.log(snake)
-
+let jogo = setInterval(iniciarJogo, 100);
 
 
 function criarBG() {
@@ -27,7 +26,22 @@ function criarCobrinha() {
 }
 
 
+document.addEventListener("keydown", update);
+
+function update(event) {
+  if (event.keyCode == 37 && direction != "rigth") direction = "left";
+  if (event.keyCode == 38 && direction != "down") direction = "up";
+  if (event.keyCode == 39 && direction != "left") direction = "rigth";
+  if (event.keyCode == 40 && direction != "up") direction = "down";
+}
+
+
 function iniciarJogo() {
+  if (snake[0].x > 15 * box && direction == "rigth") snake[0].x = 0;
+  if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+  if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+  if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
   criarBG();
   criarCobrinha();
 
@@ -51,5 +65,4 @@ function iniciarJogo() {
 
 }
 
-let jogo = setInterval(iniciarJogo, 100);
 
